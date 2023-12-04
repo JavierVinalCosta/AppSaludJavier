37461d871c70cc1061d6c395a876a47e146195b0
JAVI <javvincos@alu.edu.gva.es>
2023-12-04 18:57:57 +0100

    cambios y test bascula.js

--- test/bascula.spec.js
+++ test/bascula.spec.js
@@ -1,11 +1,11 @@
 const { expect } = require('chai');
-const Báscula = require('./Modelos/Bascula.js'); 
+const Bascula = require('./Modelos/Bascula.js'); // Asegúrate de ajustar la ruta correcta
 
-describe('Báscula', () => {
+describe('Bascula', () => {
   let bascula;
 
   beforeEach(() => {
-    bascula = new Báscula();
+    bascula = new Bascula();
   });
 
   it('debería inicializar sin registros', () => {
@@ -31,11 +31,17 @@ describe('Báscula', () => {
 
   it('debería calcular el IMC correctamente', () => {
     bascula.anotarPeso(70, 1.75);
-    expect(bascula.calcularIMC()).to.equal(22.857142857142858);
+    expect(bascula.calcularIMC()).to.equal(22.86); // Ajustado a dos decimales
   });
 
   it('debería describir el IMC correctamente', () => {
     expect(bascula.describirIMC(15)).to.equal('Infrapeso (delgadez severa)');
     expect(bascula.describirIMC(17)).to.equal('Infrapeso (delgadez moderada)');
+    expect(bascula.describirIMC(18)).to.equal('Infrapeso (delgadez aceptable)');
+    expect(bascula.describirIMC(22)).to.equal('Peso normal');
+    expect(bascula.describirIMC(28)).to.equal('Sobrepeso');
+    expect(bascula.describirIMC(32)).to.equal('Obeso (Tipo I)');
+    expect(bascula.describirIMC(37)).to.equal('Obeso (Tipo II)');
+    expect(bascula.describirIMC(40)).to.equal('Obeso (Tipo III)');
   });
 });
